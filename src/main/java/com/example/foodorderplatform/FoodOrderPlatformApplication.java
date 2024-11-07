2f89f26d23ac448803d9b8df5cbe1e53242971aa
yshong1998 <yshong1998@gmail.com>
2024-11-07 15:40:14 +0900

    initial commit

---
+++ src/main/java/com/example/foodorderplatform/FoodOrderPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.foodorderplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodOrderPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodOrderPlatformApplication.class, args);
+    }
+
+}
