1a2c0be07f7f838e3d820038386f7b24989a5f4a
yshong1998 <yshong1998@gmail.com>
2024-11-19 02:19:05 +0900

    Feat: [AI] AI스크립트 생성 기능 추가

--- src/main/java/com/example/foodorderplatform/entity/AiScript.java
+++ src/main/java/com/example/foodorderplatform/entity/AiScript.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 
 import java.util.UUID;
+import lombok.NoArgsConstructor;
 
 /*
 * check
@@ -13,16 +14,23 @@ import java.util.UUID;
 @Getter
 @Entity
 @Table(name = "p_ai_script")
+@NoArgsConstructor
 public class AiScript extends Timestamped {
 	@Id
 	@GeneratedValue(strategy = GenerationType.UUID)
 	private UUID id;
-	@Column
+	@Column(columnDefinition = "TEXT")
 	private String scriptQuestion;	// 스크립트질문
-	@Column
+	@Column(columnDefinition = "TEXT")
 	private String scriptAnswer;	// 스크립트 답변
 
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "user_id")
 	private User user;
+
+	public AiScript(String scriptQuestion, String scriptAnswer, User user){
+		this.scriptQuestion = scriptQuestion;
+		this.scriptAnswer = scriptAnswer;
+		this.user = user;
+	}
 }
